995469bed8f1c11d0ac09bf4da7fd01992179d6e
smbek <smbek.kg@gmail.com>
2024-04-06 17:49:49 +0000

    added class3 files

---
+++ Class3/main.tf
@@ -0,0 +1,40 @@
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  #   availability_zone = "us-east-2a"
+  #   subnet_id = "subnet-0a5f7e741f1d6fc9a"
+  vpc_security_group_ids      = [aws_security_group.allow_tls.id]
+  key_name                    = aws_key_pair.deployer.key_name
+  count                       = 3
+  user_data                   = file("apache.sh")
+  user_data_replace_on_change = true
+  tags                        = local.common_tags
+
+  #   tags = {
+  #     Name = "HelloWorld"
+  #     team = "DevOps"
+  #     Environtment = "Dev"
+  #   }
+}
+
+output "ec2" {
+  value = aws_instance.web[*].public_ip
+
+}
